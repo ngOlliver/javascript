f179a180296515b5a87f3a4111328b0d94acb5d9
Natã Gabriel de Oliveira <nataneioliveira32@gmail.com>
2023-11-25 15:49:55 -0300

    Testando exercício

---
+++ cursojs/aula14ex/exercício016/script.js
@@ -0,0 +1,8 @@
+function contar() {
+   var txtinicio = window.document.querySelector('input#txtinicio')
+   var txtfim = window.document.querySelector('input#txtfim')
+   var inicio = Number(txtinicio.value)
+   var fim = Number(txtfim.value)
+
+   if (inicio = )
+}
