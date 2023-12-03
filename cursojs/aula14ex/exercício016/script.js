71c5ef634e7581b084a9c211dd2b81241eb45ce6
Natã Gabriel de Oliveira <nataneioliveira32@gmail.com>
2023-12-03 19:40:53 -0300

    Criação de tabuada

--- cursojs/aula14ex/exercício016/script.js
+++ cursojs/aula14ex/exercício016/script.js
@@ -19,14 +19,14 @@ function contar() {
       if (i < f) {
          // Contagem crescente
          for (let c = i; c <= f; c += p) {
+            res.innerHTML += ` ${c} \u{1F449}`
          }
-         res.innerHTML += `\u{1F3C1}`
       } else {
          // Contagem regressiva
          for (let c = i; c >= f; c -= p) {
             res.innerHTML += ` ${c} \u{1F449}`
          }
       }
-      res.innerHTML += ` ${c} \u{1F449}`
+      res.innerHTML += ` \u{1F3C1}`
    }
 }
