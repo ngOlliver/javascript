b7494cb01edfef644b6ee7fac83557289968fa73
Natã Gabriel de Oliveira <nataneioliveira32@gmail.com>
2024-02-03 15:01:39 -0300

    Tentando resolver exercício

---
+++ exercicios/ex04/imc.js
@@ -0,0 +1,8 @@
+function calcularImc() {
+    let nome = document.querySelector('input#txtnome')
+    let altura = parseFloat(document.getElementById('numaltura').value)
+    let peso = parseFloat(document.querySelector('input#numpeso').value)
+    let res = document.querySelector('footer#analise')
+
+    
+}
