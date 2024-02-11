f1068ae38b213d07386e214199b9131274936b87
Natã Gabriel de Oliveira <nataneioliveira32@gmail.com>
2024-02-11 16:21:36 -0300

    Lâmpada JS

--- exercicios/ex04/imc.js
+++ exercicios/ex04/imc.js
@@ -1,18 +1,18 @@
 function calcularImc() {
-    let nome = document.querySelector("input#txtnome").value
-    let altura = parseFloat(document.getElementById("numaltura").value)
-    let peso = parseFloat(document.querySelector("input#numpeso").value)
+    let nome = document.querySelector("input#txtnome").value;
+    let altura = parseFloat(document.getElementById("numaltura").value);
+    let peso = parseFloat(document.querySelector("input#numpeso").value);
     let res = document.querySelector("footer#analise");
-    let imc = peso / (altura * altura)
-    let imc2 = imc.toFixed(2).replace(".", ",")
+    let imc = peso / (altura * altura);
+    let imc2 = imc.toFixed(2).replace(".", ",");
 
     if (nome === '' || isNaN(altura) || isNaN(peso)) {
-        window.alert('Por favor, preencha todos os campos!')
+        window.alert('Por favor, preencha todos os campos!');
     } else if (imc < 18.5) {
-        res.innerHTML = `O IMC de ${nome} é: ${imc2}. Você está abaixo do peso.`
+        res.innerHTML = `O IMC de ${nome} é: ${imc2}. Você está abaixo do peso.`;
     } else if (imc >= 25) {
-        res.innerHTML = `O IMC de ${nome} é: ${imc2}. Você está acima do peso.`
+        res.innerHTML = `O IMC de ${nome} é: ${imc2}. Você está acima do peso.`;
     } else {
-        res.innerHTML = `O IMC de ${nome} é: ${imc2}. Você está no peso ideal.`
+        res.innerHTML = `O IMC de ${nome} é: ${imc2}. Você está no peso ideal.`;
     }
 }
