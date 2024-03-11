c9a21a2c708170cb86ce7a81c705f4ce5f767de3
Natã Gabriel de Oliveira <nataneioliveira32@gmail.com>
2024-03-11 17:59:49 -0300

    Quase finalizado

--- exercicios/ex07/script.js
+++ exercicios/ex07/script.js
@@ -1,7 +1,22 @@
-function percentage() {
-    var tip = Number(document.querySelector('input#tip').value);
+let tipAmount = document.getElementById('restip');
 
-    document.getElementById('percentage').innerHTML = `${tip}%`;
+let totalBill = document.getElementById('resbill');
+
+function percentage() {
+    let tip = Number(document.querySelector('input#tip').value);
+
+    document.getElementById('percentage').innerText = `${tip}%`;
 }
 
-let bill = Number(document.getElementById('bill').value);
+function totalToPay() {
+    let bill = Number(document.getElementById('bill').value);
+
+    let tip = Number(document.querySelector('input#tip').value);
+
+    let theTip = bill * (tip / 100);
+
+    tipAmount.innerHTML = `${theTip.toLocaleString().replace('.', ',')}`;
+
+}
+
+window.addEventListener('keydown', totalToPay);
