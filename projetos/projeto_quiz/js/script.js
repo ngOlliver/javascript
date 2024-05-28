5356dfd5e77cae5fc0057b34c59f03c9b144796b
Natã Gabriel de Oliveira <nataneioliveira32@gmail.com>
2024-05-27 21:43:05 -0300

    Melhorando visual
    pasta de reset removida por ser quase inútil.

--- projetos/projeto_quiz/js/script.js
+++ projetos/projeto_quiz/js/script.js
@@ -5,89 +5,90 @@ let answers = [...document.querySelectorAll('button')]
 
 const questionsAnswers = [
     {
-        question: 'Qual o resultado da expressão: (7 x 2 / (2³ - 8)) / 8² - 14',
+        question: 'Qual o resultado da expressão (7 x 2 / (2³ - 8)) / 8² - 14<span>?</span>',
         options: ['Aproximadamente 1.15', '0', 'Aproximadamente -13.89', '0.14', 'Inexistente']
     },
     {
-        question: 'O Brasil é mais adepto a qual religião?',
+        question: 'O Brasil é mais adepto a qual religião<span>?</span>',
         options: ['Catolicismo', 'Cristianismo', 'Criacionismo', 'Jesus', 'O Brasil é ateu']
     },
     {
-        question: 'Em que linguagem de programação este quiz foi desenvolvido?',
+        question: 'Em que linguagem de programação este quiz foi desenvolvido<span>?</span>',
         options: ['JavaScript', 'BASIC', 'Python', 'HTML', 'WEB']
     },
     {
-        question: 'Quem nasce nos Estados Unidos da América é:',
+        question: 'Quem nasce nos Estados Unidos da América é<span>:</span>',
         options: ['Estadunidense', 'Américo', 'Europeu', 'Gringo', 'Americano']
     },
     {
-        question: 'O quinto número primo depois de 5 é:',
+        question: 'O quinto número primo depois de 5 é<span>:</span>',
         options: ['19', '5', '7', '11', '17']
     },
     {
-        question: 'O idioma oficial do Peru?',
+        question: 'O idioma oficial do Peru<span>?</span>',
         options: ['Espanhol', 'Inglês', 'Francês', 'Peruano', 'Português']
     },
     {
-        question: 'Como se escreve por extenso a 1276ª posição?',
+        question: 'Como se escreve por extenso a 1276ª posição<span>?</span>',
         options: ['Mil duzentos e setenta e seis', 'Milhonésima ducentésima setegésima sexta', '1276', 'Mil duzentos e setenta e sexto', 'Milésima ducentésima septuagésima sexta']
     },
     {
-        question: 'Quais são todos nomes de letras gregas?',
+        question: 'Quais são todos nomes de letras gregas<span>?</span>',
         options: ['Prime, Celta, A', 'Alfa, Beta, Ômega', 'Celta, Delta, Gama', 'A, O', 'Sygma, Lambida, Zetta']
     },
     {
-        question: 'Qual destes foi o filme com a maior bilheteria da história?',
+        question: 'Qual destes foi o filme com a maior bilheteria da história<span>?</span>',
         options: ['Avatar', 'Vingadores: Ultimato', 'Vingadores', 'King Kong', 'Matrix']
     },
     {
-        question: 'Qual é sinônimo de distante?',
+        question: 'Qual é sinônimo de distante<span>?</span>',
         options: ['Distância', 'Lá', 'Longe', 'Avante', 'Perto']
     },
     {
-        question: 'Líquido vital, incolor e inodoro:',
+        question: 'Líquido vital, incolor e inodoro<span>:</span>',
         options: ['Sangue', 'H2o', 'Oxigênio', 'Vitamina', 'Água']
     },
     {
-        question: 'Qual o número oposto a -15?',
+        question: 'Qual o número oposto a -15<span>?</span>',
         options: ['0', '--15', '15', '-30', '150']
     },
     {
-        question: 'Não é energia:',
+        question: 'Não é energia<span>:</span>',
         options: ['Calor', 'Eletricidade', 'Luz', 'Som', 'Matéria']
     },
     {
-        question: 'O que é o que é? Cai em pé, e corre deitado?',
+        question: 'O que é o que é... Cai em pé, e corre deitado<span>?</span>',
         options: ['Cadeirante', 'Chuva', 'Gato', 'Cobra', 'Carro']
     },
     {
-        question: 'Feminino de tigre',
+        question: 'Qual o feminino de tigre<span>?</span>',
         options: ['Tigresa', 'Tigre', 'Tigrona', 'Tigra', 'Tigrasa']
     },
     {
-        question: 'Não faz parte do corpo humano:',
+        question: 'Não faz parte do corpo humano<span>:</span>',
         options: ['Bile', 'Cloaca', 'Hemoglobina', 'Pele', 'Saliva']
     },
     {
-        question: 'Qual a escrita correta?',
+        question: 'Qual a escrita correta<span>?</span>',
         options: ['Obsessão', 'Obzessão', 'Obcessão', 'Obseção', 'Todas as alternativas estão incorretas']
     },
     {
-        question: 'O que significa "translate" em inglês?',
+        question: 'O que significa "translate" em inglês<span>?</span>',
         options: ['Translação', 'Translatar', 'Transformar', 'Tradução', 'Traduzir']
     },
     {
-        question: 'Qual URL está correta?',
+        question: 'Qual URL está correta<span>?</span>',
         options: ['Https://.com.youtube%0&23', 'htps://www.youtube', 'https://www.youtube.com', 'www.com.br', 'www.https://youtube.com//']
     },
     {
-        question: 'Qual o próximo número da sequência 11, 21, 32, 43, 55, 68...?',
+        question: 'Qual o próximo número da sequência 11, 21, 32, 43, 55, 68...<span>?</span>',
         options: ['83', '73', '100', '86', 'Impossível de saber']
     },
 ]
 //BARRA DE PROGRESSO??
 //GABARITO??
 //MAIS PERGUNTAS
+//CÓDIGO KONAMI
 const correctAnswers = [
     'Aproximadamente -13.89',
     'Cristianismo',
@@ -161,7 +162,7 @@ const doQuestion = () => {
                 askQuestion.innerHTML = 'Você acertou 10 de 10 perguntas. Parabéns! Você é um gênio!'
                 break;
             default: 
-                askQuestion.innerHTML = 'Então você respondeu a 11ª pergunta, huh? Bem esperto.'
+                askQuestion.innerHTML = 'Então você encontrou a 11ª pergunta, huh? Bem esperto.'
         }
 
         answers.forEach(button => {
@@ -218,3 +219,20 @@ const reset = () => {
 document.querySelector('button#resetButton').addEventListener('mouseup', reset)
 
 window.addEventListener('load', doQuestion())
+
+let teclas = [];
+let sequencia = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a', 'Enter'];
+
+window.addEventListener('keydown', (e) => {
+    teclas.push(e.key);
+
+    // Se a sequência de teclas for maior que a sequência desejada, remova a primeira tecla
+    if (teclas.length > sequencia.length) {
+        teclas.shift();
+    }
+
+    if (JSON.stringify(teclas) === JSON.stringify(sequencia)) {
+        doQuestion()
+        // Aqui você pode adicionar o código para acionar o evento DOM que você deseja
+    }
+});
